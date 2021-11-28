cd1f690fc567f06e3050bad6867056044c4dab57
orey <rey.olivier@gmail.com>
2021-11-28 10:24:30 +0100

    Initial commit

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+
+if [ $# -eq 0 ]
+then
+    echo "Git publish script version 1"
+    echo "Usage : publish.sh [comments without quotes]"
+    exit 0
+fi
+
+git add *
+message=$@
+git commit -a -m "$message"
+git push origin master
+
+echo "Done"
+
+
+
+
